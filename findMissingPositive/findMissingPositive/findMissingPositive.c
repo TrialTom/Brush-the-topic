03304d30c84ea0a3b2f3a1c71771a56cb5f3272d
TrialTom <Trailcat@163.com>
2022-09-08 22:36:45 +0800

    习题：缺失的第一个正数

---
+++ findMissingPositive/findMissingPositive/findMissingPositive.c
@@ -0,0 +1,35 @@
+#define _CRT_SRCURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int firstMissingPositive(int* nums, int numsSize) {
+    int i = 0;
+    int temp;
+    for (i = 0; i < numsSize; i++)
+    {
+        while (nums[i] > 0 && nums[i] <= numsSize && nums[i] != nums[nums[i]-1])
+        {
+            temp = nums[i];
+            nums[i] = nums[temp - 1];
+            nums[temp - 1] = temp;
+        }
+    }
+    for (i = 0; i < numsSize; i++)
+    {
+        if (nums[i] != i + 1)
+        {
+            return i + 1;
+        }
+        else if (nums[i] == numsSize)
+            return numsSize + 1;
+    }
+    return numsSize;
+}
+
+int main()
+{
+    int nums[] = { 3,4,-1 ,1};
+    int ret = firstMissingPositive(nums, sizeof(nums)/sizeof(nums[0]));
+    printf("%d\n", ret);
+	return 0;
+}
